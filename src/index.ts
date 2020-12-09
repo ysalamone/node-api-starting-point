80c28ae7900f09c96e804cbefb4b10f86f712892
Yohan Salamone <yohan.salamone@outlook.com>
2020-12-09 15:11:15 +0100

    connexion bdd

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import initDatabase from './db/index';
 import routes from './routes';
 
-dotenv.config();
-const app = express();
-routes(app);
+const startServer = async (): Promise<void> => {
+    dotenv.config();
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server listening on port ${process.env.PORT}`);
-});
+    const app = express();
+    await initDatabase();
+    routes(app);
+
+    app.listen(process.env.PORT, () => {
+        console.log(`Server listening on port ${process.env.PORT}`);
+    });
+};
+
+startServer();
--- src/routes/pubs.ts
+++ src/routes/pubs.ts
@@ -1,11 +1,16 @@
+// src/routes/pubs.ts
 import { Router, Request, Response, NextFunction } from 'express';
+import getModel from "../db/models";
 
 const router = Router();
 
+// renvoie un tableau avec tous les pubs
 router.get('/', async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    res.json({
-        key: 'test'
-    });
+    const pubModel = getModel('pub');
+
+    const pubs = await pubModel.find({});
+
+    res.json(pubs);
 });
 
 export default router;
